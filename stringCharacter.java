19005846eaa0800c7a6310efd362e47991f72a40
Gautam <108993041+gautamm0@users.noreply.github.com>
2022-09-10 00:20:16 +0530

    Add files via upload

---
+++ stringCharacter.java
@@ -0,0 +1,26 @@
+//program to count the special character in string and print them
+
+class stringCharacter
+{
+	public static void main(String []ar)
+	{
+		String str = "Hello*p* !@";
+		int count = 0;
+		String specialCharacter = "";
+		String removedSpecialCharacter = "";
+		  
+		for(int i=0; i<str.length(); i++)
+		{
+			if(!Character.isDigit(str.charAt(i)) && !Character.isLetter(str.charAt(i)) && !Character.isWhitespace(str.charAt(i)))
+			{	
+			    count++;
+			    specialCharacter = specialCharacter + str.charAt(i);
+			}
+			else
+			    removedSpecialCharacter = removedSpecialCharacter + str.charAt(i);	
+		}
+	    System.out.println("The string are : " + str);
+		System.out.println("There are " + count + " Special characters, these are : " + specialCharacter);
+	    System.out.println("String after removal of special character : " + removedSpecialCharacter);
+	}
+}
